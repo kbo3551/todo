925623378bc1487d50b738cbc858fe7eed295378
IntelliJ <kbo3551>
2022-06-18 14:59:22 +0900

    22.06.18 todolist 말줄임 표시

--- todo/src/dao/TodoQuery.java
+++ todo/src/dao/TodoQuery.java
@@ -11,7 +11,7 @@ public class TodoQuery {
 		DELETE_TODO ="DELETE FROM todo WHERE member_id =?";
 		SELECT_TODO_LIST_BY_DATE = "SELECT todo_no todoNo, todo_date todoDate, todo_content todoContent, create_date createDate, update_date updateDate FROM todo WHERE member_id=? AND todo_date=?";
 		INSERT_TODO = "INSERT INTO todo(member_id, todo_content, todo_date, create_date, update_date) VALUES(?,?,?,now(),now())";
-		SELECT_TODO_LIST_BY_MONTH = "SELECT todo_date todoDate, SUBSTR(todo_content,1,5) todoContent5 FROM todo WHERE member_id=? AND SUBSTR(todo_date,1,7)=? ORDER BY todo_date ASC";
+		SELECT_TODO_LIST_BY_MONTH = "SELECT todo_date todoDate, CONCAT( SUBSTR(todo_content,1,5) , '...')todoContent5 FROM todo WHERE member_id=? AND SUBSTR(todo_date,1,7)=? ORDER BY todo_date ASC";
 		DELETE_TODO_LIST ="DELETE FROM todo WHERE todo_no =?";
 		UPDATE_TODO_LIST ="UPDATE todo SET todo_content = ?, update_date =NOW()  WHERE todo_date = ? AND member_id = ? AND todo_no =? ";
 	}
